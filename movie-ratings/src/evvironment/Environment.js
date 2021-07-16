d89bfb32e72ef03d254eef83ffaf56e7c6e5e388
ADUNSW\z3521168 <ishan.jaidka@gmail.com>
2021-07-16 16:40:12 +1000

    updated cards body css

--- movie-ratings/src/evvironment/Environment.js
+++ movie-ratings/src/evvironment/Environment.js
@@ -2,6 +2,8 @@
  * This file contains all the API endpoints for retrieving and sending data
  */
 
-const APIEndpoint = 'http://131.236.87.85:5000/';
+
+// const APIEndpoint = 'http://131.236.87.85:5000/';
+const APIEndpoint = 'http://localhost:5000/';
 
 export default APIEndpoint;
